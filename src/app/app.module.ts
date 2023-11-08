9417551199ba772f1d3ee24015add66695d35d8b
abdelouali-collide <abdelouali.benkheil@collide.be>
2023-11-08 15:06:08 +0100

    Inital Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import "htf-2023-component";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
