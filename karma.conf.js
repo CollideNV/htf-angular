7ba9bf19389922e4acb7a464ffce1c25dd71d604
glenn-collide <glenn.hahn@collide.be>
2024-11-12 16:54:55 +0100

    - remove year 2023

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/htf-2023-angular'),
+      dir: require('path').join(__dirname, './coverage/htf-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import "htf-2023-component";
+import "htf-component";
 
 @NgModule({
   declarations: [
